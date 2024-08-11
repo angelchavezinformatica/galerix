26515aa55f161e463bd42d66f9bbb75660f545de
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-08-11 14:54:00 -0500

    b-add- Database select

--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,11 +1,3 @@
-from mysql import connector
+from .db import Database
 
-from config import CONFIG
-
-CONN = connector.connect(
-    user=CONFIG.get('DB_USER'),
-    password=CONFIG.get('DB_PASSWORD'),
-    host=CONFIG.get('DB_HOST'),
-    port=CONFIG.get('DB_PORT'),
-    database=CONFIG.get('DB_NAME'),
-)
+DB = Database()
---
+++ backend/src/database/db.py
@@ -0,0 +1,26 @@
+from mysql import connector
+
+from config import CONFIG
+
+
+class Database:
+    def __init__(self) -> None:
+        self.conn = connector.connect(
+            user=CONFIG.get('DB_USER'),
+            password=CONFIG.get('DB_PASSWORD'),
+            host=CONFIG.get('DB_HOST'),
+            port=CONFIG.get('DB_PORT'),
+            database=CONFIG.get('DB_NAME'),
+        )
+
+    def server_info(self):
+        if self.conn.is_connected():
+            return self.conn.get_server_info()
+
+    def select(self, sql: str, many=True):
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+
+        if many:
+            return cursor.fetchall()
+        return cursor.fetchone()
